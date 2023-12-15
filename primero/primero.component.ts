de1db1abcb1999fbe3816baa4b038dc783df6308
AshlyPineda <pinedaashly23+gmail@gmail.com>
2023-12-14 19:42:10 -0600

    first commit

---
+++ primero/primero.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+// import { Ciudad } from '../../api/datos';
+// import { CiudadService } from '../../service/ciudad.service';
+
+@Component({
+  selector: 'app-primero',
+  templateUrl: './primero.component.html',
+  styleUrls: ['./primero.component.css']
+})
+export class PrimeroComponent {
+  // ciudades: Ciudad[];
+  // ciudadSeleccionada: Ciudad;
+
+  // constructor(private ciudadService: CiudadService) {
+  //   this.ciudades = ciudadService.getCiudades();
+  // }
+}
